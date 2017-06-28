87b70208b5a5cfacaceb8bfaea013f8e17bb507d
Kavalon Wong <klovelovely@live.com>
2017-06-28 20:24:30 +0800

    npm init & 添加好友时的欢迎语

--- mybot.js
+++ mybot.js
@@ -1,7 +1,13 @@
-const { Wechaty } = require('wechaty') // import { Wechaty } from 'wechaty'
+const { Wechaty } = require('wechaty')
 
-Wechaty.instance() // Singleton
+Wechaty.instance({profile: 'kangkang'})
 .on('scan', (url, code) => console.log(`Scan QR Code to login: ${code}\n${url}`))
 .on('login',       user => console.log(`User ${user} logined`))
 .on('message',  message => console.log(`Message: ${message}`))
+.on('friend', async function(contact, request){
+  if(request){
+    await request.accept()
+    await contact.say('halo 这里是康康酱 :3 请随意~')
+  }
+})
 .init()
